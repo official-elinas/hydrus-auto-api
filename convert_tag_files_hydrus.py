83808d1919fee4e03c8287912956f922abf4a382
Elinas <support@sneerz.com>
2022-12-08 21:28:10 -0600

    remove dupe tags, non ascii characters, and only use spaces (experimental)

--- convert_tag_files_hydrus.py
+++ convert_tag_files_hydrus.py
@@ -10,20 +10,16 @@ if not os.path.exists(f"{os.getcwd()}/new_tag_files"):
     os.mkdir(f"{os.getcwd()}/new_tag_files")
 
 for file in os.listdir(original_tags_dir):
-    curr_file_tags = []
     with open(f"{original_tags_dir}/{file}", 'rb') as f:
         lines = f.readlines()
         found_tags = []
-        # no duplicate tags
-        unique_tags = set()
         for line in lines:
-            tag = line.strip().decode('utf-8').replace('_', '')
-            found_tags.append(tag)
-        for tag in found_tags:
-            if tag not in unique_tags:
-                unique_tags.add(tag)
-
+            tag = line.strip().decode('utf-8')
+            if '_' in tag:
+                tag = tag.replace('_', ' ')
+            if ':' not in tag and tag.isascii():  # and not tag.isnumeric()
+                found_tags.append(tag)
+        unique_tags = (list(dict.fromkeys(found_tags)))
         with open(f"{os.getcwd()}/new_tag_files/{file}", 'w', encoding='utf-8') as new_tag_file:
             print(f'Updating tags for file: {file}')
-            # sort tags alphabetically and separate with commas
-            new_tag_file.write((', '.join(sorted(unique_tags))))
+            new_tag_file.write((' '.join(t for t in unique_tags)))
