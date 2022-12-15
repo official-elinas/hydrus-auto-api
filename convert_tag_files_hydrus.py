07c343775b5799f02078523fd02867cbf1bad0ea
Elinas <support@sneerz.com>
2022-12-15 17:07:15 -0600

    clean up code

--- convert_tag_files_hydrus.py
+++ convert_tag_files_hydrus.py
@@ -15,37 +15,27 @@ for file in os.listdir(original_tags_dir):
         characters_idx = 1
         for line in lines:
             tag = line.strip().decode('utf-8')
-
             if '_' in tag:
                 tag = tag.replace('_', ' ')
-
             # ignore these tags
             if 'tagme' in tag:
                 continue
-
             # only add one creator tag due to frequent dupes from Hydrus
-            if 'creator:' in tag and not creator_found:
+            elif 'creator:' in tag and not creator_found:
                 # found_tags.append(tag)
                 found_tags.insert(0, tag.split(':')[1]) # remove the namespace add the artist to idx 0
                 creator_found = True
-                continue
-
             # dupe tags can happen with underscores so don't add dupe tags
-            if ('character:' in tag) and (tag != 'character:original') and tag.split(':')[1] not in found_tags: 
+            elif ('character:' in tag) and (tag != 'character:original') and tag.split(':')[1] not in found_tags:
                 # insert characters to list after artist and increment
                 found_tags.insert(characters_idx, tag.split(':')[1])
                 characters_idx += 1
-                continue
-
             # get namespaced hydrus clothing tags and remove the namespace
-            if 'clothing:' in tag:
+            elif 'clothing:' in tag:
                 found_tags.append(tag.split(':')[1])
-                continue
-
             # get rid of remaining namespaced tags except creator/character and only numeric tags
-            if ':' not in tag and not tag.isnumeric() and tag.isascii():  # and not tag.isnumeric()
+            elif ':' not in tag and not tag.isnumeric() and tag.isascii():  # and not tag.isnumeric()
                 found_tags.append(tag)
-
         unique_tags = (list(dict.fromkeys(found_tags)))
         with open(f"{os.getcwd()}/new_tag_files_commas/{file.split('.')[0]}.txt", 'w', encoding='utf-8') as new_tag_file:  # split('.'[0])}.txt
             print(f'Updating tags for file: {file.split(".")[0]}.txt')
